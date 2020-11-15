f05abeb908d6835f401c8d47449de85905d1dbf0
Conrado Pinheiro <conradopsa@gmail.com>
2020-11-15 19:07:48 -0300

    Home page finished

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import AppBar from "@material-ui/core/AppBar/AppBar";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
@@ -17,6 +18,17 @@ import {ReactComponent as Coin} from '../../assets/home/coin.svg';
 import {ReactComponent as Phone} from '../../assets/home/phone.svg';
 import {ReactComponent as New} from '../../assets/home/new.svg';
 import {ReactComponent as Cash} from '../../assets/home/cash.svg';
+import {ReactComponent as EarnCash} from '../../assets/home/earn-cashback.svg';
+import {ReactComponent as CashbackTag}  from '../../assets/home/cashback-tag.svg';
+import mercadoLivreLogo from '../../assets/home/logos/mercado-livre.png';
+import amazonLogo from '../../assets/home/logos/amazon.png';
+import kabumLogo from '../../assets/home/logos/kabum.png';
+import rennerLogo from '../../assets/home/logos/renner.png';
+import saraivaLogo from '../../assets/home/logos/saraiva.png';
+import feiraNovaLogo from '../../assets/home/logos/feira-nova.png';
+import uberLogo from '../../assets/home/logos/uber.png';
+import uberEatsLogo from '../../assets/home/logos/uber-eats.png';
+
 
 import React from "react";
 
@@ -39,7 +51,7 @@ function Home() {
 			
 			<div>
 				<p>
-					Olá Ednaldo
+					Olá Marilene
 				</p>
 				<h6>
 					Nível 1 - Mercado Pontos
@@ -106,32 +118,137 @@ function Home() {
 			</div>
 		</div>
 		
+		<div className="white-card">
+			<Coin style={{width: "188px", height: "100%"}}/>
+			<p>Use o mercado cash (M$) para comprar produtos com estabelecimentos que aceitam Mercado pago</p>
+		</div>
+		
 		<div className="white-card-list">
 			<div>
 				<IconButton aria-label="phone">
 					<Phone />
 				</IconButton>
-				<p>Recarregar celular</p>
+				<p>Recarregar celular</p>				
 			</div>
 			<div>
-				<IconButton aria-label="biometria">
-					<Biometry />
-				</IconButton>
-				<p>Biometria</p>
+				<Link to={'/biometria'}>
+					<IconButton aria-label="biometria">
+						<Biometry />
+					</IconButton>
+				</Link>
+				<New />
+				<p>Biometria</p>				
 			</div>
 			<div>
-				<IconButton aria-label="mercado cash">
-					<Cash />
-				</IconButton>
-				<p>Mercado Cash</p>
+				<Link to={'/cash'}>
+					<IconButton aria-label="mercado cash">
+						<Cash />
+					</IconButton>
+				</Link>
+				<New />
+				<p>Mercado Cash</p>				
 			</div>
 			<div>
-				<IconButton aria-label="simule seu rendimento">
-					<Graphs />
-				</IconButton>
-				<p>Simule seu Rendimento</p>
+				<Link to={'/financeiro'}>
+					<IconButton aria-label="simule seu rendimento">
+						<Graphs />
+					</IconButton>
+				</Link>
+				<New />
+				<p>Simule seu Rendimento</p>				
 			</div>
 		</div>
+		
+		<div className="white-card-with-header">
+			<header><EarnCash />Ganhe cashback!</header>
+			<main style={{textAlign: "center"}}>
+			
+				<p>Comprando nos sites</p>
+				<div className="earn-cashback-card-list white-card-list">
+					<div>
+						<IconButton>
+							<img src={mercadoLivreLogo}/>
+						</IconButton>
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>8%</p>
+						</div>
+					</div>
+					<div>
+						<IconButton>
+							<img style={{height: '15px'}} src={amazonLogo}/>
+						</IconButton>	
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>6%</p>
+						</div>
+					</div>
+					<div>
+						<IconButton>
+							<img style={{height: '25px'}} src={kabumLogo}/>
+						</IconButton>	
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>6%</p>
+						</div>
+					</div>
+					<div>
+						<IconButton>
+							<img style={{height: '17px'}} src={rennerLogo}/>
+						</IconButton>
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>5%</p>
+						</div>
+					</div>
+					<div>
+						<IconButton>
+							<img style={{height: '8px'}} src={saraivaLogo}/>
+						</IconButton>
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>5%</p>
+						</div>
+					</div>
+					<div>
+						<IconButton>
+							<img style={{height: '15px'}} src={feiraNovaLogo}/>
+						</IconButton>
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>4%</p>
+						</div>
+					</div>
+				</div>
+				<br/>
+				<p>Comprando nos aplicativos</p>
+				<div style={{gridTemplateColumns: '1fr 1fr', marginLeft: '50px', marginRight: '50px'}} className="earn-cashback-card-list white-card-list">
+					<div>
+						<IconButton aria-label="phone">
+							<img style={{height: '10px'}} src={uberLogo}/>
+						</IconButton>
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>4%</p>
+						</div>
+						
+					</div>
+					<div>
+						<IconButton aria-label="phone">
+							<img style={{height: '17px'}} src={uberEatsLogo}/>
+						</IconButton>
+						<div className="cashback-tag">
+							<CashbackTag/>
+							<p>4%</p>
+						</div>
+					</div>
+				</div>
+				
+				<div className="card-list">
+				</div>
+			</main>
+		</div>
+		
 	  </main>
     </section>
   );
