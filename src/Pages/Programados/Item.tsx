154cf307ff4774635b803b37bf5b73f018753439
gabrielsynaii <gabrielsynaii@gmail.com>
2020-11-15 20:37:19 -0300

    Fixed cards style

--- src/Pages/Programados/Item.tsx
+++ src/Pages/Programados/Item.tsx
@@ -1,11 +1,45 @@
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import { Card, CardActions, CardContent, Grid, IconButton, Typography } from '@material-ui/core';
+import AttachMoney from '@material-ui/icons/AttachMoney';
+import MoneyOff from '@material-ui/icons/MoneyOff';
+import Edit from '@material-ui/icons/Edit';
 
-export default styled.div`
-  justify-content: center;
-  align-items: center;
-  height: 50px;
-  display: -webkit-box;
-  width: 50px;
-  color: #fff;
-  border-radius: 100px;
+const Content = styled(CardContent)`
+    padding: 1vh 2vw;
+    &:last-child {
+        padding-bottom: 1vh;
+    }
 `;
+
+function Item(props: any) {
+    return (
+        <>
+            <Card variant="outlined">
+                <Content>
+                    <Grid container direction="row" wrap="nowrap" alignItems="center" spacing={2}>
+                        <Grid item>
+                            {props.category ? <MoneyOff style={{fill: 'red'}} /> : <AttachMoney style={{fill: 'green'}} /> }
+                        </Grid>
+
+                        <Grid item style={{flex: '1'}}>
+                            <p>{props.description}</p>
+                            <Typography variant="subtitle1" component="p">R$ {props.value}</Typography>
+                            <p style={{fontSize: '0.8em'}}>{props.category ? 'DESPESA' : 'RECEITA'}</p>
+                        </Grid>
+
+                        <Grid item>
+                            <CardActions style={{float: 'right'}}>
+                                <IconButton>
+                                    <Edit />
+                                </IconButton>
+                            </CardActions>
+                        </Grid>
+                    </Grid>
+                </Content>
+            </Card>
+        </>
+    );
+}
+
+export default Item;
--- src/Pages/Programados/Programados.tsx
+++ src/Pages/Programados/Programados.tsx
@@ -1,128 +1,156 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import './style.tsx';
-import { AppBar, Typography, IconButton, Menu, MenuItem, Toolbar, Button } from '@material-ui/core';
+import { Link } from "react-router-dom";
+import Carousel from "react-elastic-carousel";
+
+import { AppBar, Typography, IconButton, Toolbar, Button, Grid, Container, Box, Fab } from '@material-ui/core';
 import Share from '@material-ui/icons/Share';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import useStyles from './style';
 import TrendingUp from '@material-ui/icons/TrendingUp';
-import TrendingDown from '@material-ui/icons/TrendingDown';
-import AttachMoney from '@material-ui/icons/AttachMoney';
-import MoneyOff from '@material-ui/icons/MoneyOff';
-import Edit from '@material-ui/icons/Edit';
 import FilterList from '@material-ui/icons/FilterList';
-import Carousel from "react-elastic-carousel";
-import Item from "./Item";
-import { StyledDiv } from "./style";
+import Add from '@material-ui/icons/Add';
+
+import useStyles, { StyledDiv } from "./style";
+import CarouselItem from "./CarouselItem";
+import Item from './Item';
+
+const DADOS = [
+    {
+        category: 0,
+        description: 'SALÁRIO MENSAL',
+        value: 2752.29
+    },
+    {
+        category: 1,
+        description: 'EMPRÉSTIMO',
+        value: 222.90
+    },
+    {
+        category: 1,
+        description: 'EMPRÉSTIMO',
+        value: 550.31
+    }
+];
+
+const greenColor = "#77C87F";
+const redColor = "#FF9090";
 
 function MeuFinanceiro() {
     const classes = useStyles();
-    const greenColor: string = "#77C87F";
-    const redColor: string = "#FF9090";
 
     return (
         <>
             <AppBar position="static">
-                <Toolbar className={classes.toolbar}>
-                    <IconButton edge="start" component={Link} to="/financeiro" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <ChevronLeft />
-                    </IconButton>
-                    
-                    <Typography variant="h6" className={classes.title}>
-                        Transações Programadas
-                    </Typography>
-                    
-                    <IconButton color="inherit">
-                        <Share
-                            aria-label="compartilhar"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            color="inherit"
-                            className={classes.menuIcon}
-                        >
-                            <Share />
-                        </Share>
-                    </IconButton>
+                <Toolbar>
+                    <Grid container direction="row" alignItems="center">
+                        <Grid item>
+                            <IconButton edge="start" component={Link} to="/financeiro" className={classes.menuButton} color="inherit" aria-label="menu">
+                                <ChevronLeft />
+                            </IconButton>
+                        </Grid>
+
+                        <Grid item style={{flex: '1'}}>
+                            <Typography variant="subtitle1" align="center">
+                                Transações Programadas
+                            </Typography>
+                        </Grid>
+
+                        <Grid item>
+                            <IconButton color="inherit">
+                                <Share
+                                    aria-label="compartilhar"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    color="inherit"
+                                    className={classes.menuIcon}
+                                >
+                                    <Share />
+                                </Share>
+                            </IconButton>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </AppBar>
 
-            <div className={classes.saldoTitleContent}>
-                <p className={classes.saldoTitle}>
-                    Saldo em Conta
-                </p>
-                <p className={classes.valorSaldo}>
-                    R$ 2.397,98
-                </p>
-                <p className={classes.valorSaldoCash}>
-                    M$ 1.523,00
-                </p>
-            </div>
+            <Box my={2}>
+                <Grid container direction="column" alignItems="center">
+                    <Grid item>
+                        <Typography color="primary" variant="subtitle2">Saldo em Conta</Typography>
+                    </Grid>
 
-            <div className={classes.totals}>
-                <div className={classes.boxTotalLeft}>
-                    <div className={classes.totalLeftIcon}>
-                        <TrendingUp />
-                    </div>
-                    <div className={classes.totalLeft}>
-                        <p className={classes.totalLeftTitle}>Tot. Entradas</p>
-                        <p className={classes.totalLeftValue}>R$XXX,XX</p>
-                    </div>
-                </div>
-                <div className={classes.boxTotalRight}>
-                    <div className={classes.totalRightIcon}>
-                        <TrendingDown />
-                    </div>
-                    <div className={classes.totalRight}>
-                        <p className={classes.totalRightTitle}>Tot. Saídas</p>
-                        <p className={classes.totalRightValue}>R$XXX,XX</p>
-                    </div>
-                </div>
-            </div>
+                    <Grid item>
+                        <Typography color="primary" variant="h4">R$ {(DADOS.filter(({category}) => category == 0).reduce((acc, {value}) => (acc + value), 0) - DADOS.filter(({category}) => category == 1).reduce((acc, {value}) => (acc + value), 0)).toFixed(2)}</Typography>
+                    </Grid>
 
-            <StyledDiv className={classes.carousel}>
-                <Carousel itemsToScroll={5} itemsToShow={5} className={classes.carousel} showArrows={true} pagination={false}>
-                    <Item style={{ backgroundColor: greenColor }}>One</Item>
-                    <Item style={{ backgroundColor: redColor }}>Two</Item>
-                    <Item style={{ backgroundColor: redColor }}>Three</Item>
-                    <Item style={{ backgroundColor: redColor }}>Four</Item>
-                    <Item style={{ backgroundColor: redColor }}>Five</Item>
-                    <Item style={{ backgroundColor: greenColor }}>Six</Item>
-                    <Item style={{ backgroundColor: greenColor }}>Seven</Item>
-                    <Item style={{ backgroundColor: greenColor }}>Eight</Item>
-                </Carousel>
-            </StyledDiv>
+                    <Grid item>
+                        <p style={{fontSize: '0.8em'}}>M$ 1.523,00</p>
+                    </Grid>
+                </Grid>
+            </Box>
 
-            <div className={classes.editOptions}>
-                <Button className={classes.buttonAdd}>ADICIONAR</Button>
-                <Button className={classes.settings}>
-                    <FilterList fontSize="small" className={classes.settingIcon} />
-                </Button>
-            </div>
+            <Box my={2}>
+                <Grid container direction="row" wrap="nowrap" justify="space-evenly" style={{width: '100%', maxWidth: '400px', margin: '0 auto'}}>
+                    <Grid item>
+                        <Grid container direction="row" wrap="nowrap" alignItems="center" spacing={2} style={{border: '1px solid #ccc'}}>
+                            <Grid item>
+                                <TrendingUp style={{fill: 'green'}} />
+                            </Grid>
+
+                            <Grid item>
+                                <Typography>Tot. Entradas</Typography>
+                                <Typography>R$ {DADOS.filter(({category}) => category == 0).reduce((acc, {value}) => (acc + value), 0).toFixed(2)}</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
             
-            <div>
-                <div className={classes.card}>
-                    <div className={classes.cardValue}>
-                        <div className={classes.cardIcon}>
-                            <AttachMoney fontSize="large" />
-                        </div>
-                        <div className={classes.totalLeft}>
-                            <p className={classes.totalLeftTitle}>05/ABR</p>
-                            <p className={classes.totalLeftValue}>R$XXX,XX</p>
-                        </div>
-                    </div>
-                    <div className={classes.cardDetail}>
-                        <p className={classes.totalLeftTitle}>SALÁRIO MENSAL</p>
-                    </div>
-                    <Button className={classes.iconEdit}>
-                        <Edit fontSize="small" style={{ color: "black", opacity: "50%" }} />
-                    </Button>
-                </div>
-            </div>
+                    <Grid item>
+                        <Grid container direction="row" wrap="nowrap" alignItems="center" spacing={2} style={{border: '1px solid #ccc'}}>
+                            <Grid item>
+                                <TrendingUp style={{fill: 'red'}} />
+                            </Grid>
+
+                            <Grid item>
+                                <Typography>Tot. Saídas</Typography>
+                                <Typography>R$ {DADOS.filter(({category}) => category == 1).reduce((acc, {value}) => (acc + value), 0).toFixed(2)}</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Box>
+
+            <Box my={2} style={{maxWidth: '600px', margin: '0 auto'}}>
+                <StyledDiv className={classes.carousel}>
+                    <Carousel itemsToScroll={5} itemsToShow={5} className={classes.carousel} showArrows={true} pagination={false}>
+                        <CarouselItem style={{ backgroundColor: greenColor }}>One</CarouselItem>
+                        <CarouselItem style={{ backgroundColor: redColor }}>Two</CarouselItem>
+                        <CarouselItem style={{ backgroundColor: redColor }}>Three</CarouselItem>
+                        <CarouselItem style={{ backgroundColor: redColor }}>Four</CarouselItem>
+                        <CarouselItem style={{ backgroundColor: redColor }}>Five</CarouselItem>
+                        <CarouselItem style={{ backgroundColor: greenColor }}>Six</CarouselItem>
+                        <CarouselItem style={{ backgroundColor: greenColor }}>Seven</CarouselItem>
+                        <CarouselItem style={{ backgroundColor: greenColor }}>Eight</CarouselItem>
+                    </Carousel>
+                </StyledDiv>
+            </Box>
+
+            <Container maxWidth="sm">
+                <Grid container direction="row" alignItems="center" justify="space-between">
+                    <Grid item>
+                        <Button color="primary">ADICIONAR</Button>
+                    </Grid>
+
+                    <Grid item>
+                        <IconButton color="primary">
+                            <FilterList fontSize="small" />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+
+                <Grid container direction="column" spacing={2}>
+                    {
+                        DADOS.map((item, index) => (<Grid item><Item key={index} {...item} /></Grid>))
+                    }
+                </Grid>
+            </Container> 
         </>
     );
 }
